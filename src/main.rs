1aff7390270f6810f181aa22f6daecd93520c3bb
dusan <dusan.miloradovic@gmail.com>
2025-12-02 17:34:08 +0400

    first try no result

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod rotate_count_zeros;
 mod repeating_sequence;
+mod repeating_sequence_multiple;
 
 fn main() {
-    //rotate_count_zeros::times_zero_dial("/home/dusan/rust_learn/Advent_of_code_2025/rotate_zero_count_puzzle_input.txt");
-    repeating_sequence::calculate_invalid_in_range();
+   // rotate_count_zeros::times_zero_dial("/home/dusan/rust_learn/Advent_of_code_2025/rotate_zero_count_puzzle_input.txt");
+    //repeating_sequence::calculate_invalid_in_range();
+    repeating_sequence_multiple::calculate_invalid_in_range();
 }
---
+++ src/repeating_sequence_multiple.rs
@@ -0,0 +1,86 @@
+use std::cmp::{max, min};
+use std::fs::read_to_string;
+
+fn invalid_in_range_with_digits(
+    left_range: &str,
+    right_range: &str,
+    digits: usize,
+) -> Option<Vec<u128>> {
+    let base: u128 = 10;
+    let mut normalized_left = left_range.to_string();
+    let mut normalized_right = right_range.to_string();
+    let satisfies_left = normalized_left.len() % digits == 0;
+    let satisfies_right = normalized_right.len() % digits == 0;
+    let mut ret: Vec<u128> = vec![];
+    if (!satisfies_left && !satisfies_right) {
+        // Here we rely on problem input, there will be no differences in keys with two orders of mangiture
+        // (two digits more), the max diff is one digit, so if none of this is satisified we return None
+        return None;
+    }
+    if (!satisfies_left) {
+        normalized_left = base.pow(right_range.len() as u32 - 1).to_string();
+    }
+    if (!satisfies_right) {
+        normalized_right = (base.pow(left_range.len() as u32) - 1).to_string();
+    }
+
+    let left_bound = normalized_left[..digits].parse::<u128>().unwrap();
+    let right_bound = normalized_right[..digits].parse::<u128>().unwrap();
+
+    let mut no_of_digits = normalized_left.len();
+    loop {
+        let repeats = no_of_digits / digits;
+        for x in left_bound..right_bound {
+            let mut candidate = "".to_string();
+            for i in 0..repeats {
+                candidate.push_str(x.to_string().as_str());
+            }
+            let can_num = candidate.parse::<u128>().unwrap();
+            if can_num >= left_bound && can_num <= right_bound {
+                ret.push(can_num);
+            }
+        }
+        no_of_digits += 1;
+        if no_of_digits > normalized_right.len() {
+            break;
+        }
+    }
+    Some(ret)
+}
+
+fn invalid_in_range(left_range: &str, right_range: &str) -> Option<Vec<u128>> {
+    let mut ret: Vec<u128> = vec![];
+    let max_digits = max(left_range.len(), right_range.len());
+    for i in 1..min(1, max_digits / 2) {
+        let r = invalid_in_range_with_digits(left_range, right_range, i);
+        match r {
+            Some(v) => {
+                for _v in v {
+                    ret.push(_v);
+                }
+            }
+            None => {}
+        }
+    }
+    Some(ret)
+}
+
+pub fn calculate_invalid_in_range() {
+    let file_lines = read_to_string("./puzzle_input_day2.txt").unwrap();
+    let no_whitespace: String = file_lines.chars().filter(|c| !c.is_whitespace()).collect();
+    let fs = no_whitespace.split(",");
+    let mut ranges: Vec<(&str, &str)> = vec![];
+    for f in fs {
+        let gg = f.split("-").collect::<Vec<&str>>();
+        ranges.push((gg[0], gg[1]));
+    }
+    for (l, r) in ranges {
+        print!("For range {l}..{r}\n");
+        let inval = invalid_in_range(l, r);
+        if let Some(v) = inval {
+            for _v in v {
+                print!("{_v}\n");
+            }
+        }
+    }
+}
