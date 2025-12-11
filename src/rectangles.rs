74a5766981277c951662b997cd7892e1bad956bd
dusan <dusan.miloradovic@gmail.com>
2025-12-11 10:58:54 +0400

    stry on mac

--- src/rectangles.rs
+++ src/rectangles.rs
@@ -24,21 +24,20 @@ fn remove_point(points: &[(i128, i128)], point: (i128, i128)) -> Vec<(i128, i128
     v
 }
 
-fn remove_multiple_points (points: &[(i128, i128)], points_to_remove: &[(i128, i128)]) -> Vec<(i128, i128)>{
+fn remove_multiple_points(points: &[(i128, i128)], points_to_remove: &[(i128, i128)]) -> Vec<(i128, i128)> {
     let mut pp = points.to_vec();
-    for p in points_to_remove{
+    for p in points_to_remove {
         pp = remove_point(&pp, *p);
     }
     pp
 }
-fn construct_elf_walk(points: &[(i128, i128)])  {
-
+fn construct_elf_walk(points: &[(i128, i128)]) {
     let mut vertical_direction = WalkDirection::Down;
     //once we react the bottom, we start going up
     let mut the_walk = ElfWalk::new();
-    let mut remaining_points = points.clone().to_vec();
-    let mut max_area:i128 = 0;
-    let mut points_to_remove:Vec<(i128,i128)> = Vec::new();
+    let mut remaining_points = points.to_vec();
+    let mut points_to_remove: Vec<(i128, i128)> = Vec::new();
+    let mut multiple_bounds: Vec<Vec<(i128, i128)> > = Vec::new();
     // The points are ordered by the row number of the input, and from the first point we are going right and down
     loop {
         //break here if no more points
@@ -53,15 +52,13 @@ fn construct_elf_walk(points: &[(i128, i128)])  {
                 && point.0 == initial_point.0
                 && point.1 == initial_point.1
             {
-               // remaining_points = remove_point(&remaining_points, point);
+                // remaining_points = remove_point(&remaining_points, point);
                 points_to_remove.push(point);
 
-               // ret_walks.push(the_walk);
-                let max_walk_area =
-                    do_the_elf_walk(initial_point, &remaining_points, the_walk, last_row_number);
-                if max_walk_area>max_area{
-                    max_area=max_walk_area;
-                }
+                // ret_walks.push(the_walk);
+                let bounds =
+                    do_the_elf_walk(initial_point,  the_walk, last_row_number);
+                multiple_bounds.push(bounds);
                 the_walk = ElfWalk::new();
                 remaining_points = remove_multiple_points(&remaining_points, &points_to_remove);
                 points_to_remove = Vec::new();
@@ -92,7 +89,7 @@ fn construct_elf_walk(points: &[(i128, i128)])  {
                     match points.into_iter().find(|p| p.1 > point.1 && p.0 == point.0) {
                         None => {
                             vertical_direction = WalkDirection::Up;
-                           // remaining_points = remove_point(&remaining_points, point);
+                            // remaining_points = remove_point(&remaining_points, point);
                             points_to_remove.push(point);
                         }
                         Some(p) => {
@@ -111,12 +108,10 @@ fn construct_elf_walk(points: &[(i128, i128)])  {
                 } else {
                     match points.into_iter().find(|p| p.1 < point.1 && p.0 == point.0) {
                         None => {
-                           // ret_walks.push(the_walk);
-                            let max_walk_area =
-                                do_the_elf_walk(initial_point, &remaining_points, the_walk, last_row_number);
-                            if max_walk_area>max_area{
-                                max_area=max_walk_area;
-                            }
+                            // ret_walks.push(the_walk);
+                            let bounds =
+                                do_the_elf_walk(initial_point,  the_walk, last_row_number);
+                          multiple_bounds.push(bounds);
                             the_walk = ElfWalk::new();
                             //remaining_points = remove_point(&remaining_points, point);
                             points_to_remove.push(point);
@@ -126,7 +121,7 @@ fn construct_elf_walk(points: &[(i128, i128)])  {
                         }
                         Some(p) => {
                             let dist = (point.1 - p.1).abs() as usize;
-                           // remaining_points = remove_point(&remaining_points, point);
+                            // remaining_points = remove_point(&remaining_points, point);
                             points_to_remove.push(point);
                             point = *p;
                             let step = ElfWalkStep {
@@ -141,22 +136,23 @@ fn construct_elf_walk(points: &[(i128, i128)])  {
             }
         }
     }
-    print!("max area : {max_area}");
+    let painted_area = paint_area(&points ,multiple_bounds);
+   // print!("multiple bounds\n{:?}",multiple_bounds);
 }
 pub fn brute_force() {
     let points = get_input();
-    let mut maxSurface: i128 = 0;
+    let mut max_surface: i128 = 0;
     for i in 0..points.len() {
         for j in i..points.len() {
             let a = points[i];
             let b = points[j];
             let surface = ((a.0 - b.0).abs() + 1) * ((a.1 - b.1).abs() + 1);
-            if surface > maxSurface {
-                maxSurface = surface;
+            if surface > max_surface {
+                max_surface = surface;
             }
         }
     }
-    print!("Max surface = {maxSurface}\n");
+    print!("Max surface = {max_surface}\n");
 }
 
 fn get_input() -> Vec<(i128, i128)> {
@@ -209,16 +205,9 @@ fn rect_in_bounds(point_a: (i128, i128), point_b: (i128, i128), bounds: &[(i128,
 }
 fn do_the_elf_walk(
     starting_point: (i128, i128),
-    points: &[(i128, i128)],
     elf_walk: ElfWalk,
     last_row_number: i128,
-) -> i128 {
-    //returns max area
-    let mut max_area: i128 = 0;
-    // let points = get_input();
-    // let (point, last_row_number) = find_the_upper_left_point(&points);
-    //let elf_walk = construct_elf_walk(&point, &points);
-    // print!("Debug elf walk {:#?}", elf_walk);
+) -> Vec<(i128, i128)> {
     let mut bounds: Vec<(i128, i128)> = vec![(-1, -1); (last_row_number + 1) as usize];
     let el0 = &elf_walk[0];
     let right_point = starting_point.0 + el0.moves as i128;
@@ -229,7 +218,7 @@ fn do_the_elf_walk(
     for j in 1..elf_walk.len() {
         let e = &elf_walk[j];
         if e.direction == WalkDirection::Down {
-            for j in 0..e.moves {
+            for _ in 0..e.moves {
                 current_row += 1;
                 let mut ex_bound = bounds[current_row].clone();
                 ex_bound.1 = current_column;
@@ -259,7 +248,7 @@ fn do_the_elf_walk(
             bounds[current_row] = ex_bound;
         }
         if e.direction == WalkDirection::Up {
-            for j in 0..e.moves {
+            for _ in 0..e.moves {
                 current_row -= 1;
                 let mut ex_bound = bounds[current_row].clone();
                 ex_bound.0 = current_column;
@@ -268,23 +257,82 @@ fn do_the_elf_walk(
         }
     }
     //print!("Bounds\n{:?}", bounds);
-    for i in 0..points.len() {
-        for j in i..points.len() {
-            let p1 = points[i];
-            let p2 = points[j];
-            let area = rect_in_bounds(p1, p2, &bounds);
-            if area != -1 {
-                // print!("Satisfied {:?}.. {:?}, {area}\n", p1, p2);
-                if area > max_area {
-                    max_area = area;
-                }
+    // for i in 0..points.len() {
+    //     for j in i..points.len() {
+    //         let p1 = points[i];
+    //         let p2 = points[j];
+    //         let area = rect_in_bounds(p1, p2, &bounds);
+    //         if area != -1 {
+    //             // print!("Satisfied {:?}.. {:?}, {area}\n", p1, p2);
+    //             if area > max_area {
+    //                 max_area = area;
+    //             }
+    //         }
+    //     }
+    // }
+  //  print!("Max walk area = {max_area}\n");
+    bounds
+}
+
+fn print_paint_area(area:&Vec<Vec<bool>>){
+    for row in area{
+        for c in row{
+            if *c ==false{
+                print!(".");
+            }else {
+                print!("X");
+            }
+        }
+        print!("\n")
+    }
+}
+fn paint_area(points:&[(i128, i128)], multiple_bounds:Vec<Vec<(i128, i128)> >  )->Vec<Vec<bool>>{
+    let mut ret:Vec<Vec<bool>> = Vec::new();
+    let mut xs:Vec<i128>=Vec::new();
+    let mut ys:Vec<i128>=Vec::new();
+    for p in points{
+        xs.push(p.0);
+        ys.push(p.1);
+    }
+    xs.sort_by(|a,b| b.cmp(a));
+    ys.sort_by(|a,b| b.cmp(a));
+    let w =xs[0]+1;
+    let h=ys[0]+1;
+    for _ in 0..h{
+        let v :Vec<bool>=vec![false;w as usize];
+        ret.push(v);
+    }
+    for bounds in multiple_bounds{
+        for i in 0..bounds.len(){
+            let bound =bounds[i];
+            if bound == (-1,-1) {
+                continue;
+            }
+            let left_bound = bound.0;
+            let right_bound = bound.1;
+            if left_bound>w || right_bound>w || left_bound<0 || right_bound<0{
+               // print!("Should not happen {left_bound} {right_bound}");
+                continue;
+            }
+            let row=&mut ret[i];
+            if left_bound == -1{
+                row[right_bound as usize]=true;
+                continue;
+            }
+            if right_bound ==-1{
+                row[left_bound as usize]=true;
+                continue;
+            }
+            for b in left_bound..=right_bound{
+                row[b as usize]=true;
             }
         }
     }
-    print!("Max walk area = {max_area}\n");
-    max_area
+    print_paint_area(&ret);
+    ret
 }
-pub fn help_the_elves(){
+
+pub fn help_the_elves() {
     let points = get_input();
     construct_elf_walk(&points);
 }
